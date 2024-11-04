7a5e30948bb3263ae80848b347f5638ad56f54e0
Veli Dogan <velidogan120@gmail.com>
2024-11-04 15:47:03 +0300

    d

--- src/socket.js
+++ src/socket.js
@@ -2,6 +2,6 @@
 
 import { io } from "socket.io-client";
 
-export const socket = io("https://kanban-nextjs-production.up.railway.app:8080", { transports: ["websocket","polling"],
+export const socket = io("https://kanban-nextjs-production.up.railway.app", { transports: ["websocket","polling"],
   withCredentials: true,
   });
